994a6e3fe1c14bb7960181f6755e666837b8c98c
kayleelovesdata <kay.lau.garcia@gmail.com>
2026-08-15 22:46:09 +0000

    Wrote Unit Tests and Included Requested Screenshot

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,41 @@
 import pytest
 # TODO: add necessary import
-
+from ml.model import (compute_model_metrics, inference, save_model, load_model, train_model)
+import numpy as np
 # TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_compute_model_metrics():
     """
-    # add description for the first test
+   Check that precision, recall, and F1 equal the expected values
     """
     # Your code here
-    pass
-
+    y = np.array([1, 1, 0, 0])
+    preds = np.array([1, 0, 1, 0])
+    precision, recall, fbeta = compute_model_metrics(y, preds)
+    assert precision == 0.5
+    assert recall == 0.5
+    assert fbeta == 0.5
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_save_and_load_model(tmp_path):
     """
-    # add description for the second test
+    # Verifies pickle serialization
     """
     # Your code here
-    pass
+    orig_object = {"model": "random_forest", "random_state": 19}
+    file_path = tmp_path / "test_model.pkl"
+    save_model(orig_object, file_path)
+    loaded_object = load_model(file_path)
+    assert loaded_object == orig_object
 
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_train_model_and_inference():
     """
-    # add description for the third test
+    # Checks that training and prediction work together as expected
     """
     # Your code here
-    pass
+    X = np.array([[0], [1], [2], [3]])
+    y = np.array([0, 0, 1, 1])
+    trained = train_model(X, y)
+    preds = inference(trained, X)
+    assert len(preds) == len(y)
